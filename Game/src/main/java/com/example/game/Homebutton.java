5ffddd8719ce68665acd7174557f9604e8904b14
Kushagra20075 <Kushagra20075@iiitd.ac.in>
2021-12-04 12:59:09 +0530

    Initial GUI done

--- Game/src/main/java/com/example/game/Homebutton.java
+++ Game/src/main/java/com/example/game/Homebutton.java
@@ -1,33 +1,31 @@
 package com.example.game;
 
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.File;
 
-public class StartButton extends Button {
+public class Homebutton extends Button {
     private final String STYLE = "-fx-background-color:transparent; -fx-background-size: cover";
-    private final String STYLE_Closed = "-fx-background-color:transparent; -fx-background-size: cover";
-    private String path = "src/main/resources/com/example/game/images/resume.png";
-    private View_Manager manager;
+    private final String STYLE_Closed = "-fx-background-color:transparent; -fx-background-size: cover;";
+    private String path = "src/main/resources/com/example/game/images/home.png";
+    private Stage stage;
 
-    StartButton(View_Manager manager){
-        this.manager = manager;
+    Homebutton(Stage stage){
+        this.stage = stage;
         setText("");
-        setPrefHeight(110);
-        setPrefWidth(110);
+        setPrefHeight(50);
+        setPrefWidth(50);
         setStyle(STYLE);
         initialisebutton();
         ImageView img = new ImageView(new File(path).toURI().toString());
-        img.setFitHeight(110);
-        img.setFitWidth(110);
+        img.setFitHeight(50);
+        img.setFitWidth(50);
         this.setGraphic(img);
     }
     private void click(){
@@ -40,14 +38,8 @@ public class StartButton extends Button {
     }
 
 
-    private void handler() {
-        LoadPage page = new LoadPage(manager.getMainStage());
-        manager.removevideo();
-        page.start();
-    }
-
-
     private void initialisebutton(){
+
         setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
@@ -88,7 +80,11 @@ public class StartButton extends Button {
         });
     }
 
-
+    private void handler() {
+        View_Manager manager = new View_Manager(this.stage);
+        this.stage.setScene(manager.getMainScene());
+        stage.show();
+    }
 
 
 }
--- Game/src/main/java/com/example/game/ResumeButton.java
+++ Game/src/main/java/com/example/game/ResumeButton.java
@@ -1,33 +1,28 @@
 package com.example.game;
 
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 import java.io.File;
 
-public class StartButton extends Button {
+public class ResumeButton extends Button {
     private final String STYLE = "-fx-background-color:transparent; -fx-background-size: cover";
-    private final String STYLE_Closed = "-fx-background-color:transparent; -fx-background-size: cover";
+    private final String STYLE_Closed = "-fx-background-color:transparent; -fx-background-size: cover;";
     private String path = "src/main/resources/com/example/game/images/resume.png";
-    private View_Manager manager;
 
-    StartButton(View_Manager manager){
-        this.manager = manager;
+    ResumeButton(){
         setText("");
-        setPrefHeight(110);
-        setPrefWidth(110);
+        setPrefHeight(50);
+        setPrefWidth(50);
         setStyle(STYLE);
         initialisebutton();
         ImageView img = new ImageView(new File(path).toURI().toString());
-        img.setFitHeight(110);
-        img.setFitWidth(110);
+        img.setFitHeight(50);
+        img.setFitWidth(50);
         this.setGraphic(img);
     }
     private void click(){
@@ -40,21 +35,7 @@ public class StartButton extends Button {
     }
 
 
-    private void handler() {
-        LoadPage page = new LoadPage(manager.getMainStage());
-        manager.removevideo();
-        page.start();
-    }
-
-
     private void initialisebutton(){
-        setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                handler();
-            }
-        });
-
         setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
@@ -87,8 +68,6 @@ public class StartButton extends Button {
             }
         });
     }
-
-
 
 
 }
