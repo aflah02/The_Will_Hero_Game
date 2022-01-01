54107268ff266388cd5817788e3933302def6146
Aflah <72096386+aflah02@users.noreply.github.com>
2022-01-01 14:53:21 +0530

    Done

--- Game/src/main/java/com/example/game/analyzeStats.py
+++ Game/src/main/java/com/example/game/analyzeStats.py
@@ -2,7 +2,7 @@ import pandas as pd
 from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
-
+# fig, ax = plt.subplots()
 with open("heroLocations.txt", "r") as f:
     data = f.readlines()
 ls = []
@@ -16,6 +16,7 @@ for i in data:
     else:
         c += 1
         ls.append(i.strip().split(' '))
+lsdata = [1,2,3,4,5,6,7,8]
 lsx = []
 lsy = []
 lsp = []
@@ -41,21 +42,47 @@ plt.ylabel("Y-Coordinate")
 plt.plot(lsx, lsy)
 # plt.ylim([74.5, 75.5])
 # show plot
+textstr = '\n'.join((
+    r'Orcs Killed=%d' % (lsdata[0], ),
+    r'TNTs Burst=%d' % (lsdata[1], ),
+    r'Score Achieved=%d' % (lsdata[2], ),
+    r'Total Time Taken=%d' % (lsdata[3], ),
+    r'Number of Times Orcs were Encountered=%d' % (lsdata[4], ),
+    r'Number of Swords Collected=%d' % (lsdata[5], ),
+    r'Number of Spears Collected=%d' % (lsdata[6], ),
+    r'Number of Coin Chests Opened=%d' % (lsdata[7], ),
+    ))
+# ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14,
+#         verticalalignment='top', bbox=props)
+plt.text(0, 0, textstr, fontsize = 10)
 plt.show()
 plt.savefig('location.png')
 
 
- # Creating figure
+#  Creating figure
 fig = plt.figure(figsize = (10, 7))
 # ax = plt.axes(projection ="3d")
 # Creating plot
 # ax.scatter3D(lsx, lsy, lsp, color = "green")
 plt.title("Location of Player wrt Points")
 plt.xlabel("X-Coordinate")
-plt.ylabel("Points")
-plt.plot(lsx, lsp)
+plt.ylabel("Y-Coordinate")
+plt.plot(lsx, lsy)
 # plt.ylim([74.5, 75.5])
 # show plot
+textstr = '\n'.join((
+    r'Orcs Killed=%d' % (lsdata[0], ),
+    r'TNTs Burst=%d' % (lsdata[1], ),
+    r'Score Achieved=%d' % (lsdata[2], ),
+    r'Total Time Taken=%d' % (lsdata[3], ),
+    r'Number of Times Orcs were Encountered=%d' % (lsdata[4], ),
+    r'Number of Swords Collected=%d' % (lsdata[5], ),
+    r'Number of Spears Collected=%d' % (lsdata[6], ),
+    r'Number of Coin Chests Opened=%d' % (lsdata[7], ),
+    ))
+# ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14,
+#         verticalalignment='top', bbox=props)
+plt.text(70, 360, textstr, fontsize = 7)
 plt.show()
 plt.savefig('point.png')
 
